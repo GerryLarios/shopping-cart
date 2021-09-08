60b82235f50b50f9f654331efb39af5527e82663
Gerardo Larios <gerardo.larios69@gmail.com>
2021-09-08 09:35:08 -0500

    Feat[SHOPPING-CART-11] add product mock ups

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,42 @@
 export const GITHUB_PROFILE_URL = 'https://github.com/GerryLarios'
 export const BASE_URL_API = 'https://fakestoreapi.com'
+export const BASE_PATH_IMAGES = '/static/images'
+
+export const MOCK_PRODUCTS = [
+  {
+    id: 1,
+    price: 60,
+    title: 'SSD 480 GB',
+    category: 'technology'
+  },
+  {
+    id: 2,
+    price: 953.90,
+    title: 'MacBook pro 16GB RAM',
+    category: 'technology'
+  },
+  {
+    id: 3,
+    price: 19.99,
+    title: 'Skinny Jeans',
+    category: 'clothes'
+  },
+  {
+    id: 4,
+    price: 125,
+    title: 'Antisocial social club tshirt',
+    category: 'clothes'
+  },
+  {
+    id: 5,
+    price: 30.12,
+    title: 'Ozs Silver',
+    category: 'jewelry'
+  },
+  {
+    id: 6,
+    price: 299.99,
+    title: 'NBA Championship Rings',
+    category: 'jewelry'
+  }
+]
